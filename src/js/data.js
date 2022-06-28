0e6c8e3f51243dad4ea76d36b2d5dc2954c8d8e9
Mahuva <108345609+Mahuva@users.noreply.github.com>
2022-06-28 18:40:06 +0530

    Create data.js

---
+++ src/js/data.js
@@ -0,0 +1,35 @@
+"use-strict"
+
+const data =
+{
+    // ======== Usually English words =====
+    "hi": "Hi..",
+    "hello": "Hello",
+    "good": "Good",
+    "oye": "Oye ? ",
+    "how are you": "I am fine, how are you ?",
+    "i am fine": "That is good, i am also fine",
+    "who are you": "i am a Robot 1.0.0 Made By Pruthviraj",
+    "what is your name": "i am Robot made by Pruthviraj",
+    "fine": "That is Good, i am aslo fine",
+    "hullo": "Hello,..",
+    "hallo": "Hello...",
+    "good morning": "Good Morning",
+    "good evening": "Good Evening",
+    "good night": "Good Night, take enough rest",
+    "good bye": "why ? ok. Good Bye",
+    "help me": "which type of help do you want ? ",
+    "i am so tired": "i am also tired but we should work continuously",
+    "wow": "what happened ?",
+    "hei": "Hei ... what are you doing ? ",
+    "well": "well, what well ?",
+    // ======================================
+
+    // ======= Time And Date =====
+    "year": "you are Living in => " + new Date().getFullYear(),
+    "what is time": "Time is  : " + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds(),
+    "show me time": "Time is  : " + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds(),
+    "time now": "Time is  : " + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds(),
+
+    // ======= end of Time and Date ====
+}
